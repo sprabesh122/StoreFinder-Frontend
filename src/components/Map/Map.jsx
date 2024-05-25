247eb25390f6131e4d2f7e4ebbe5c2d0e3b1e212
sprabesh122 <sprabesh122@gmail.com>
2024-05-25 11:43:33 +0530

    integrated map successfully

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,113 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import 'ol/ol.css';
+import { Map as OLMap, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+import { toLonLat } from 'ol/proj';
+import { Feature } from 'ol';
+import { Point } from 'ol/geom';
+import { Style, Icon } from 'ol/style';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 
 const Map = () => {
+  const [userLocation, setUserLocation] = useState(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        error => {
+          console.error("Error getting user's location:", error);
+          setUserLocation({ latitude: 52.70967533219885, longitude: -8.020019531250002 });
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+      setUserLocation({ latitude: 52.70967533219885, longitude: -8.020019531250002 });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (userLocation) {
+      const olMap = new OLMap({
+        target: mapRef.current,
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        view: new View({
+          center: fromLonLat([userLocation.longitude, userLocation.latitude]),
+          zoom: 14,
+        }),
+      });
+
+      const userMarker = new Feature({
+        geometry: new Point(fromLonLat([userLocation.longitude, userLocation.latitude])),
+      });
+
+      userMarker.setStyle(new Style({
+        image: new Icon({
+          color: '#0000ff',
+          crossOrigin: 'anonymous',
+          src: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><circle cx="32" cy="32" r="32" fill="blue" /></svg>',
+          scale: 0.1,
+        }),
+      }));
+
+      const vectorSource = new VectorSource({
+        features: [userMarker],
+      });
+
+      const vectorLayer = new VectorLayer({
+        source: vectorSource,
+      });
+
+      olMap.addLayer(vectorLayer);
+
+      const overpassUrl = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:1000,${userLocation.latitude},${userLocation.longitude})[shop];out;`;
+
+      axios.get(overpassUrl).then(response => {
+        const shops = response.data.elements;
+        shops.forEach(shop => {
+          console.log(shop);
+          const shopMarker = new Feature({
+            geometry: new Point(fromLonLat([shop.lon, shop.lat])),
+          });
+
+          shopMarker.setStyle(new Style({
+            image: new Icon({
+              color: '#ff0000',
+              crossOrigin: 'anonymous',
+              src: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><circle cx="32" cy="32" r="32" fill="red" /></svg>',
+              scale: 0.1,
+            }),
+          }));
+
+          vectorSource.addFeature(shopMarker);
+        });
+      }).catch(error => {
+        console.error('Error fetching nearby stores:', error);
+      });
+    }
+  }, [userLocation]);
+
   return (
     <div>
-      <iframe 
-        width="100%" 
-        height="600" 
-        src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=52.70967533219885, -8.020019531250002&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" 
-        frameBorder="0" 
-        scrolling="no" 
-        marginHeight="0" 
-        marginWidth="0"
-        title="Google Map"
-      ></iframe>
+      {userLocation ? (
+        <div ref={mapRef} style={{ width: '100%', height: '600px' }}></div>
+      ) : (
+        <p>Loading map...</p>
+      )}
     </div>
   );
-}
+};
 
 export default Map;
